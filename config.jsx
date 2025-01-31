9687e34774d96f9e9e227ac9b4716e98888481c6
Rob Blasetti <rblasett@gmail.com>
2025-01-31 17:53:08 +1100

    config file

--- config.jsx
+++ config.jsx
@@ -1,6 +1,6 @@
 const devAPI = 'http://localhost:5005';
 const stagingAPI = 'https://liquid-spirit-backend-staging-2a7049350332.herokuapp.com';
 
-const API_URL = devAPI;
+const API_URL = stagingAPI;
 
 export default API_URL;
