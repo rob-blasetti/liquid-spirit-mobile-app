9f374f2bd2d44a5b8a09d0f38a34d6c3ce063f82
Rob Blasetti <rblasett@gmail.com>
2025-02-07 13:16:51 +1100

    android app setup

--- config.jsx
+++ config.jsx
@@ -1,6 +1,7 @@
 const devAPI = 'http://localhost:5005';
 const stagingAPI = 'https://liquid-spirit-backend-staging-2a7049350332.herokuapp.com';
+const productionAPI = 'https://liquid-spirit-backend-prod-34ac4484898d.herokuapp.com';
 
-const API_URL = stagingAPI;
+const API_URL = productionAPI;
 
 export default API_URL;
