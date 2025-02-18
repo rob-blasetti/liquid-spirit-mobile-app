d267c83315929f952b9d91b068b95a9018c32738
Rob Blasetti <rblasett@gmail.com>
2025-02-18 15:33:59 +1100

    API back to prod

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import Config from 'react-native-config';
 
 //Set the API URL
-export const API_URL = Config.DEV_API;
+export const API_URL = Config.PROD_API;
 
 export const AWS_ID = Config.AWS_ACCESS_KEY_ID;
 export const AWS_Secret = Config.AWS_SECRET_ACCESS_KEY;
